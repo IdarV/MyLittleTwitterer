a5b64e5b80bfcebc1c4e7a160f79fc5960a2afcb
IdarV <tjoida13@nith.no>
2015-04-07 14:40:31 +0200

    added post-time to tweet info

--- twitter_fetch.rb
+++ twitter_fetch.rb
@@ -5,10 +5,11 @@ require 'htmlentities'
 
 rss = SimpleRSS.parse open('http://twitrss.me/twitter_user_to_rss/?user=IdarVassdal')
 rss.entries.each do |entry|
+  date_time = '(' + entry.pubDate.strftime('%d %b %Y, %H:%M') + ')'
   #seperate tweet text and author (first colon does the job)
   tweet = entry.title.to_s.split(':', 2)
   #Run through HTMLEntities to make pretty. Start all new lines with tab
-  puts HTMLEntities.new.decode (tweet.first + "\n\t" + tweet.last.gsub("\n", "\n\t"))
+  puts HTMLEntities.new.decode (tweet.first + ' ' + date_time + "\n\t" + tweet.last.gsub("\n", "\n\t"))
 end
 
 
