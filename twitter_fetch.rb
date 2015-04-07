790b8c293e73277f6ed99ba40615683fd669830b
IdarV <tjoida13@nith.no>
2015-04-07 15:18:23 +0200

    app is now has simple shoes GUI

--- twitter_fetch.rb
+++ twitter_fetch.rb
@@ -2,17 +2,32 @@ require 'rubygems'
 require 'open-uri'
 require 'simple-rss'
 require 'htmlentities'
+require 'green_shoes'
 
 puts 'Twitter username: '
-user = gets
+#user = gets
+user = 'IdarVassdal'
+index = 0
 
 rss = SimpleRSS.parse open('http://twitrss.me/twitter_user_to_rss/?user=' + user)
-rss.entries.each do |entry|
-  date_time = '(' + entry.pubDate.strftime('%d %b %Y, %H:%M') + ')'
-  #seperate tweet text and author (first colon does the job)
-  tweet = entry.title.to_s.split(':', 2)
-  #Run through HTMLEntities to make pretty. Start all new lines with tab
-  puts HTMLEntities.new.decode (tweet.first + ' ' + date_time + "\n\t" + tweet.last.gsub("\n", "\n\t"))
-end
+Shoes.app(title: 'myLittleTweeter', width: 600, height: 900) do
+  rss.entries.each do |entry|
+    date_time = "(#{entry.pubDate.strftime('%d %b %Y, %H:%M')})"
+    #seperate tweet text and author (first colon does the job)
+    tweet = entry.title.to_s.split(':', 2)
+    #Run through HTMLEntities to make pretty. Start all new lines with tab
+    text = HTMLEntities.new.decode (tweet.first + ' ' + date_time + "\n\t" + tweet.last.gsub("\n", "\n\t"))
+    flow do
+      if index.even?
+        background lightblue
+      else
+        background lightgreen
+      end
+      index += 1
+      para text
+    end
+  end
 
+
+end
 
