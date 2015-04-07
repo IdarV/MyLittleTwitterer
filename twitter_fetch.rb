640cb863779fa8ad1375610aef19b11d7062d699
IdarV <tjoida13@nith.no>
2015-04-07 15:33:53 +0200

    GUI now has inputfield and button with no functionality

--- twitter_fetch.rb
+++ twitter_fetch.rb
@@ -11,6 +11,13 @@ index = 0
 
 rss = SimpleRSS.parse open('http://twitrss.me/twitter_user_to_rss/?user=' + user)
 Shoes.app(title: 'myLittleTweeter', width: 600, height: 900) do
+  flow do
+    @editline = edit_line
+    @push = button 'Fetch tweets'
+    @push.click {
+
+    }
+  end
   rss.entries.each do |entry|
     date_time = "(#{entry.pubDate.strftime('%d %b %Y, %H:%M')})"
     #seperate tweet text and author (first colon does the job)
@@ -24,7 +31,7 @@ Shoes.app(title: 'myLittleTweeter', width: 600, height: 900) do
         background lightgreen
       end
       index += 1
-      para text
+      @para = para text
     end
   end
 
