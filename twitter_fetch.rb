8e88e87c92a3fb0c44ef53866c947077a34b0b74
IdarV <tjoida13@nith.no>
2015-04-07 14:11:08 +0200

    fixed comments in fetcher

--- twitter_fetch.rb
+++ twitter_fetch.rb
@@ -5,7 +5,7 @@ require 'htmlentities'
 
 rss = SimpleRSS.parse open('http://twitrss.me/twitter_user_to_rss/?user=IdarVassdal.json')
 rss.entries.each do |entry|
-  #seperate tweet text and author
+  #seperate tweet text and author (first colon does the job)
   tweet = entry.title.to_s.split(':', 2)
   #Run through HTMLEntities to make pretty. Start all new lines with tab
   puts HTMLEntities.new.decode (tweet.first + "\n\t" + tweet.last.gsub("\n", "\n\t"))
