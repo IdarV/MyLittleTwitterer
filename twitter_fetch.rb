fb1951d6e9ade2ffb9aaf75643e4c818558f1386
IdarV <tjoida13@nith.no>
2015-04-07 14:29:15 +0200

    fixed rss uri

--- twitter_fetch.rb
+++ twitter_fetch.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 require 'simple-rss'
 require 'htmlentities'
 
-rss = SimpleRSS.parse open('http://twitrss.me/twitter_user_to_rss/?user=IdarVassdal.json')
+rss = SimpleRSS.parse open('http://twitrss.me/twitter_user_to_rss/?user=IdarVassdal')
 rss.entries.each do |entry|
   #seperate tweet text and author (first colon does the job)
   tweet = entry.title.to_s.split(':', 2)
