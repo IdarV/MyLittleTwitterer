33ce91e7ef8eda518113f86f9ed7d098cce760cc
IdarV <tjoida13@nith.no>
2015-04-07 16:39:35 +0200

    removed HTMLEntities dependency

--- twitter_fetch.rb
+++ twitter_fetch.rb
@@ -1,30 +1,32 @@
 require 'rubygems'
 require 'open-uri'
 require 'simple-rss'
-require 'htmlentities'
 require 'green_shoes'
 
 puts 'Program started'
 $user = 'justinbieber'
 
 def get_rss_feed_as_string(name)
+
   puts 'fetching RSS for user ' + name
+
   begin
     rss = SimpleRSS.parse open('http://twitrss.me/twitter_user_to_rss/?user=' + name)
   rescue
     return 'User not found'
   end
-  puts '..successful'
+
+  puts "\t..successful"
   all_tweets = ''
   rss.entries.each do |entry|
     date_time = "(#{entry.pubDate.strftime('%d %b %Y, %H:%M')})"
     #seperate tweet text and author (first colon does the job)
     tweet = entry.title.to_s.split(':', 2)
-    #Run through HTMLEntities to make pretty. Start all new lines with tab
     text = tweet.first + ' ' + date_time + "\n\t" + tweet.last.gsub("\n", "\n\t")
     all_tweets += text + "\n\n"
   end
-  puts all_tweets
+
+  puts 'Served RSS to GUI'
   all_tweets.to_s
 end
 
